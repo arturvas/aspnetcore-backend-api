eee7aa334cc0a09dc597fa933659af7f903538a0
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2026-02-15 10:32:15 -0300

    chore: configure JWT authentication and authorization in `Program.cs`

--- CadastroProdutos/Program.cs
+++ CadastroProdutos/Program.cs
@@ -1,16 +1,39 @@
+using System.Text;
 using CadastroProdutos.Infrastructure;
 using CadastroProdutos.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<MyDbContext>(options => 
+builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseSqlite("Data Source = Produtos.db"));
 
 builder.Services.AddScoped<IProdutosService, ProdutosService>();
+
+var jwtConfig = builder.Configuration.GetSection("Jwt");
+var key = Encoding.ASCII.GetBytes(jwtConfig["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtConfig["Issuer"],
+        ValidAudience = jwtConfig["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
 
 var app = builder.Build();
 
@@ -20,6 +43,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
